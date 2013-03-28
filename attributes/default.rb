9e6e78f1959d6123b5430c32257755fbb691bc5f
Josh Yotty <jyotty@bluebox.net>
2013-03-28 13:29:52 -0700

    foodcritic snobbery

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,7 @@
-default[:drbd][:remote_host] = nil
 default['drbd']['resource'] = 'r0'
-default[:drbd][:disk] = nil
-default[:drbd][:mount] = nil
-default[:drbd][:fs_type] = "ext3"
-default[:drbd][:dev] = "/dev/drbd0"
-default[:drbd][:master] = false
-default[:drbd][:port] = 7789
-default[:drbd][:configured] = false
+default['drbd']['disk'] = nil
+default['drbd']['mount'] = nil
+default['drbd']['fs_type'] = "ext3"
+default['drbd']['dev'] = "/dev/drbd0"
+default['drbd']['master'] = false
+default['drbd']['port'] = 7789
--- recipes/pair.rb
+++ recipes/pair.rb
@@ -31,8 +31,8 @@ end
 
 #first pass only, initialize drbd
 execute "drbdadm create-md #{node['drbd']['resource']}" do
-  subscribes :run, resources(:template => "/etc/drbd.d/#{node['drbd']['resource']}.res")
-  notifies :restart, resources(:service => "drbd"), :immediate
+  subscribes :run, resources("template[/etc/drbd.d/#{node['drbd']['resource']}.res]")
+  notifies :restart, 'service[drbd]', :immediate
   only_if do
     cmd = Chef::ShellOut.new("drbd-overview")
     overview = cmd.run_command
@@ -44,14 +44,14 @@ end
 
 #claim primary based off of node['drbd']['master']
 execute "drbdadm -- --overwrite-data-of-peer primary all" do
-  subscribes :run, resources(:execute => "drbdadm create-md #{node['drbd']['resource']}")
+  subscribes :run, resources("execute[drbdadm create-md #{node['drbd']['resource']}]")
   only_if { node['drbd']['master'] && !node['drbd']['configured'] }
   action :nothing
 end
 
 #You may now create a filesystem on the device, use it as a raw block device
 execute "mkfs -t #{node['drbd']['fs_type']} #{node['drbd']['dev']}" do
-  subscribes :run, resources(:execute => "drbdadm -- --overwrite-data-of-peer primary all")
+  subscribes :run, resources("execute[drbdadm -- --overwrite-data-of-peer primary all]")
   only_if { node['drbd']['master'] && !node['drbd']['configured'] }
   action :nothing
 end
@@ -74,6 +74,6 @@ ruby_block "set drbd configured flag" do
   block do
     node.set['drbd']['configured'] = true
   end
-  subscribes :create, resources(:execute => "mkfs -t #{node['drbd']['fs_type']} #{node['drbd']['dev']}")
+  subscribes :create, resources("execute[mkfs -t #{node['drbd']['fs_type']} #{node['drbd']['dev']}]")
   action :nothing
 end
